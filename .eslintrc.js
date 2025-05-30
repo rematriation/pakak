a485cccc10aed9a4d3630e4fe280d0eb0d3a2ede
Daksh Pratap Singh <dsingh23@ncsu.edu>
2025-05-30 02:35:45 -0400

    chore(scaffold): add initial project structure

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",    // TypeScript rules
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+  env: {
+    node: true,
+    es2020: true,
+    jest: true
+  },
+  rules: {
+    // project-specific tweaks:
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "off"
+  }
+};
---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+  testMatch: ["**/tests/**/*.test.ts"],
+    coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/tests/",
+    "src/libs/"
+  ],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json"
+    }
+  }
+};
